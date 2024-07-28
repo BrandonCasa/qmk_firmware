6adcd4ec8f34bb96ecbbc3dfc3d610babe16178d
BrandonCasa <brandoncasa8@gmail.com>
2024-07-27 20:29:26 -0400

    custom keymap.c

--- keyboards/gmmk/pro/rev1/ansi/keymaps/last_input_priority/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/last_input_priority/keymap.c
@@ -16,6 +16,84 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    L_W = SAFE_RANGE,
+    L_A,
+    L_S,
+    L_D
+};
+
+bool w_pressed = false;
+bool a_pressed = false;
+bool s_pressed = false;
+bool d_pressed = false;
+
+uint16_t last_horizontal_key = 0;
+uint16_t last_vertical_key = 0;
+
+void update_key_state(uint16_t keycode, bool pressed) {
+    switch (keycode) {
+        case L_W:
+            w_pressed = pressed;
+            if (pressed) last_vertical_key = L_W;
+            break;
+        case L_A:
+            a_pressed = pressed;
+            if (pressed) last_horizontal_key = L_A;
+            break;
+        case L_S:
+            s_pressed = pressed;
+            if (pressed) last_vertical_key = L_S;
+            break;
+        case L_D:
+            d_pressed = pressed;
+            if (pressed) last_horizontal_key = L_D;
+            break;
+    }
+
+    if (w_pressed && !s_pressed) {
+        register_code(KC_W);
+        unregister_code(KC_S);
+    } else if (!w_pressed && s_pressed) {
+        register_code(KC_S);
+        unregister_code(KC_W);
+    } else if (!w_pressed && !s_pressed) {
+        unregister_code(KC_W);
+        unregister_code(KC_S);
+    }
+
+    if (a_pressed && !d_pressed) {
+        register_code(KC_A);
+        unregister_code(KC_D);
+    } else if (!a_pressed && d_pressed) {
+        register_code(KC_D);
+        unregister_code(KC_A);
+    } else if (!a_pressed && !d_pressed) {
+        unregister_code(KC_A);
+        unregister_code(KC_D);
+    }
+
+    if (!pressed) {
+        if (keycode == last_horizontal_key) {
+            if (a_pressed && last_horizontal_key == L_A) register_code(KC_A);
+            if (d_pressed && last_horizontal_key == L_D) register_code(KC_D);
+        }
+        if (keycode == last_vertical_key) {
+            if (w_pressed && last_vertical_key == L_W) register_code(KC_W);
+            if (s_pressed && last_vertical_key == L_S) register_code(KC_S);
+        }
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (keycode == L_W || keycode == L_A || keycode == L_S || keycode == L_D) {
+        update_key_state(keycode, record->event.pressed);
+        return false;
+    }
+    return true;
+}
+
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -41,8 +119,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_TAB,  KC_Q,    L_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
+        KC_CAPS, L_A,    L_S,    L_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -66,3 +144,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
 #endif
+
+void matrix_scan_user(void) {
+    // Add any periodic tasks here if needed
+}
